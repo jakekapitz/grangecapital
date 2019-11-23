58b51178d1c20c2887c6bd17bf84330b661e48a6
Jake Kapitz <inbox@jakekapitz.com>
2019-11-23 16:50:23 +1100

    About page updated

--- src/pages/About.vue
+++ src/pages/About.vue
@@ -1,7 +1,13 @@
 <template>
   <Layout>
     <PageTitleBar :title=title></PageTitleBar>
-    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Error doloremque omnis animi, eligendi magni a voluptatum, vitae, consequuntur rerum illum odit fugit assumenda rem dolores inventore iste reprehenderit maxime! Iusto.</p>
+    <div class="container mx-auto flex items-center">
+      <div class="w-2/5 px-8">
+        <p class="mb-2"><strong>Grange Capital Holdings Pty Ltd (“Grange Capital”)</strong> is the private investment and capital placement vehicle of Carlos Sala and Leigh Smith. Grange Capital provides investment solutions to mid-sized companies in Australia.  Carlos and Leigh have extensive experience establishing, leading and running investment businesses covering debt, high-yield, restructuring and special situations in Australia, Asia and Europe for market leading institutions.</p>
+        <p class="mb-2">Grange Capital’s telecommunications team “G Telecom” specialises in delivering “best in class” fibre solutions to both Greenfield and Brownfield sites across Australia</p>
+      </div>
+      <g-image src="~/assets/images/about.jpg" />
+    </div>
   </Layout>
 </template>
 
