7313e0a99a1c4827e5fa77c34301a7f69007801a
Jake <inbox@jakekapitz.com>
2019-12-04 10:34:24 +1100

    Mobile nav spacing

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -27,27 +27,27 @@
                 </ul>
             </nav>
             <nav class="flex lg:hidden font-display text-xl text-gray-800 font-bold" id="mobileNav">
-                <font-awesome :icon="['fas', 'bars']" v-if="!isOpen" @click="toggleMenu" size="lg" class="text-gc-dark-grey"/>
-                <font-awesome :icon="['fas', 'times']" v-if="isOpen" @click="toggleMenu" size="lg" class="text-gc-dark-grey"/>
-                <ul v-if="isOpen" class="flex flex-col w-64 bg-white py-2 mt-32 w-full absolute top-0 left-0 shadow">
-                    <li @click="toggleMenu" class="mx-8 pl-4 pt-2 pb-1 hover:text-gc-yellow transition-color border-b"><g-link to="/">Home</g-link></li>
-                    <li class="mx-8 pl-4 py-2 hover:text-gc-yellow transition-color border-b">
+                <font-awesome :icon="['fas', 'bars']" v-if="!isOpen" @click="toggleMenu" size="2x" class="mr-4 text-gc-dark-grey"/>
+                <font-awesome :icon="['fas', 'times']" v-if="isOpen" @click="toggleMenu" size="2x" class="mr-4 text-gc-dark-grey"/>
+                <ul v-if="isOpen" class="flex flex-col w-64 bg-white mt-32 w-full absolute top-0 left-0 shadow-lg">
+                    <li @click="toggleMenu" class="mx-8 pl-4 py-4 hover:text-gc-yellow transition-color border-b"><g-link to="/">Home</g-link></li>
+                    <li class="mx-8 pl-4 py-4 hover:text-gc-yellow transition-color border-b">
                         <g-link @click="toggleMenu" to="/business/">Our Business</g-link>
-                        <font-awesome :icon="['fas', 'plus']" size="sm" v-if="!submenuOpen" @click="toggleSubmenu" class="text-gc-yellow hover:text-gc-red cursor-pointer ml-6"/>
-                        <font-awesome :icon="['fas', 'minus']" size="sm" v-if="submenuOpen" @click="toggleSubmenu" class="text-gc-yellow hover:text-gc-red cursor-pointer ml-6"/>
+                        <font-awesome :icon="['fas', 'chevron-down']" size="sm" v-if="!submenuOpen" @click="toggleSubmenu" class="text-gc-dark-grey hover:text-gc-yellow cursor-pointer ml-6"/>
+                        <font-awesome :icon="['fas', 'chevron-up']" size="sm" v-if="submenuOpen" @click="toggleSubmenu" class="text-gc-dark-grey hover:text-gc-yellow cursor-pointer ml-6"/>
                         <ul v-if="submenuOpen">
-                            <li @click="toggleMenu" class="ml-8 pt-2 pb-2 text-gc-dark-grey hover:text-gc-yellow transition-color"><g-link to="/business/#seniorDebtFinance">Senior Debt Finance</g-link></li>
-                            <li @click="toggleMenu" class="ml-8 pb-2 text-gc-dark-grey hover:text-gc-yellow transition-color"><g-link to="/business/#mezzanineFinance">Mezzanine Finance</g-link></li>
-                            <li @click="toggleMenu" class="ml-8 pb-2 text-gc-dark-grey hover:text-gc-yellow transition-color"><g-link to="/business/#developmentSales">Development Sales</g-link></li>
-                            <li @click="toggleMenu" class="ml-8 pb-2 text-gc-dark-grey hover:text-gc-yellow transition-color"><g-link to="/business/#grangeCapitalInvestmentFund">Grange Capital Investment Fund</g-link></li>
-                            <li @click="toggleMenu" class="ml-8 pb-2 text-gc-dark-grey hover:text-gc-yellow transition-color"><g-link to="/business/#gcImfDevelopmentFund">IMF Development Fund</g-link></li>
-                            <li @click="toggleMenu" class="ml-8 pb-2 text-gc-dark-grey hover:text-gc-yellow transition-color"><g-link to="/business/#gTelecom">Grange Telecom</g-link></li>
+                            <li @click="toggleMenu" class="ml-4 py-2 border-b text-gc-dark-grey hover:text-gc-yellow transition-color"><g-link to="/business/#seniorDebtFinance">Senior Debt Finance</g-link></li>
+                            <li @click="toggleMenu" class="ml-4 py-2 border-b text-gc-dark-grey hover:text-gc-yellow transition-color"><g-link to="/business/#mezzanineFinance">Mezzanine Finance</g-link></li>
+                            <li @click="toggleMenu" class="ml-4 py-2 border-b text-gc-dark-grey hover:text-gc-yellow transition-color"><g-link to="/business/#developmentSales">Development Sales</g-link></li>
+                            <li @click="toggleMenu" class="ml-4 py-2 border-b text-gc-dark-grey hover:text-gc-yellow transition-color"><g-link to="/business/#grangeCapitalInvestmentFund">Grange Capital Investment Fund</g-link></li>
+                            <li @click="toggleMenu" class="ml-4 py-2 border-b text-gc-dark-grey hover:text-gc-yellow transition-color"><g-link to="/business/#gcImfDevelopmentFund">IMF Development Fund</g-link></li>
+                            <li @click="toggleMenu" class="ml-4 py-2 text-gc-dark-grey hover:text-gc-yellow transition-color"><g-link to="/business/#gTelecom">Grange Telecom</g-link></li>
                         </ul>
                     </li>
-                    <li @click="toggleMenu" class="mx-8 pl-4 py-1 hover:text-gc-yellow transition-color border-b"><g-link to="/about/">About</g-link></li>
-                    <li @click="toggleMenu" class="mx-8 pl-4 py-1 hover:text-gc-yellow transition-color border-b"><g-link to="/team/">Our Team</g-link></li>
-                    <li @click="toggleMenu" class="mx-8 pl-4 py-1 hover:text-gc-yellow transition-color border-b"><g-link to="/news/">News</g-link></li>
-                    <li @click="toggleMenu" class="mx-8 pl-4 pt-1 hover:text-gc-yellow transition-color"><g-link to="/contact/">Contact</g-link></li>
+                    <li @click="toggleMenu" class="mx-8 pl-4 py-4 hover:text-gc-yellow transition-color border-b"><g-link to="/about/">About</g-link></li>
+                    <li @click="toggleMenu" class="mx-8 pl-4 py-4 hover:text-gc-yellow transition-color border-b"><g-link to="/team/">Our Team</g-link></li>
+                    <li @click="toggleMenu" class="mx-8 pl-4 py-4 hover:text-gc-yellow transition-color border-b"><g-link to="/news/">News</g-link></li>
+                    <li @click="toggleMenu" class="mx-8 pl-4 py-4 hover:text-gc-yellow transition-color"><g-link to="/contact/">Contact</g-link></li>
                 </ul>
             </nav>
         </div>
